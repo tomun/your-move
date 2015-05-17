398004d48ab5a720046350a59d2c8759c02af6d2
Tom Underhill <tunderhill@gmail.com>
2015-05-17 13:41:13 -0700

    added welcome page and tests

--- spec/features/create_session_spec.rb
+++ spec/features/create_session_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe "creating a session" do
   context "before signing in" do
-    it "has certain links" do
+    before do
       visit "/"
+    end
+
+    it "has certain links" do
       expect(page).to have_link "Home"
       expect(page).to have_link "Login"
       expect(page).to have_link "Register"
+    end
+
+    it "tells the user to Login or Register" do
+      expect(page).to have_content "Login or Register to start playing"
     end
   end
 
@@ -20,6 +27,10 @@ describe "creating a session" do
     it "has certain links" do
       expect(page).to have_link "Home"
       expect(page).to have_link "Logout"
+    end
+
+    it "welcomes the user by name" do
+      expect(page).to have_content "Welcome #{player.handle}"
     end
 
   end
