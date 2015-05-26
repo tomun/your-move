be66b39418315207a82d49e6eebba87edbd29ba5
GGadow <gpg@gregory-gadow.net>
2015-05-25 18:30:20 -0700

    Saving work

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,17 @@
 module GamesHelper
+
+  def opponent_handle(player_id: 0, game_id: 0)
+    g = Game.find(game_id)
+    return nil unless g
+
+    result = nil
+
+    if g.player_1_id == player_id
+      result = g.player_2_id
+    elsif g.player_2_id == player_id
+      result = g.player_1_id
+    end
+
+    result
+  end
 end
