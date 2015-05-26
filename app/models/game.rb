612d6ab8568fec051c7d74b5632c7a600e35d314
Tom Underhill <tunderhill@gmail.com>
2015-05-26 09:27:29 -0700

    Two player game play is working

--- app/models/game.rb
+++ app/models/game.rb
@@ -18,14 +18,51 @@ class Game < ActiveRecord::Base
     @game_obj
   end
 
-  def board_html
-    game_obj.board_html "#{id}"
+  def board_html current_player
+    player_no = 0
+    if current_player
+      if current_player.id == player_1_id
+        player_no = 1
+      elsif current_player.id == player_2_id
+        player_no = 2
+      end
+    end
+
+    game_obj.board_html id, player_no
   end
 
   def move params
     game_obj.move params
   end
   
+  def player_1
+    Player.find(player_1_id)
+  end
+
+  def player_2
+    Player.find(player_2_id)
+  end
+
+  def moving_player
+    if game_obj.moving_player == 1
+      player_1
+    elsif game_obj.moving_player == 2
+      player_2
+    else
+      nil
+    end
+  end
+
+  def winning_player
+    if game_obj.winning_player == 1
+      player_1
+    elsif game_obj.winning_player == 2
+      player_2
+    else
+      nil
+    end
+  end
+
   # a little hack to allow game creation in the game index page
   def default_game_type
     GameType.game_types.first
