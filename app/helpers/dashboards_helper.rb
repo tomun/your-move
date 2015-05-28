28162cb42f3ceade2f9e2206c97dc0acbb3d5ce6
GGadow <gpg@gregory-gadow.net>
2015-05-27 21:28:51 -0700

    Done, I think

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,7 +1,7 @@
 module DashboardsHelper
 
-  def turn_class(game, player_id)
-    if your_turn?(game.id, player_id)
+  def turn_class(game_id, player_id)
+    if your_turn?(game_id, player_id)
       @class = "your_turn"
     else
       @class = "not_your_turn"
