913a18021700df40954f27b920996bdf0743cabd
GGadow <gpg@gregory-gadow.net>
2015-05-31 22:51:04 -0700

    Done, I think

--- app/mailers/game_mailer.rb
+++ app/mailers/game_mailer.rb
@@ -1,2 +1,11 @@
 class GameMailer < ApplicationMailer
+  include PlayersHelper
+
+  def notify_turn(game_id, player_id, your_id)
+    @game_type = GameType.find(Game.find(game_id).game_type_id)
+    @player = Player.find(player_id)
+    @opponent = Player.find(your_id)
+    mail(to: @player.email, subject: "Now it's your turn!")
+  end 
+
 end
