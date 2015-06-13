c1da46b7fce5f433b0cccd75bc8ce6508937e0bf
Tom Underhill <tunderhill@gmail.com>
2015-06-12 23:49:00 -0700

    got notifcations working reliably

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,6 +4,19 @@ describe Game, type: :model do
 
   let(:game) { create(:game) }
 
+  def move_to_player_1_win
+    # player 1 move
+    game.move ({ row: 0, col: 0 })
+    # player 2 move
+    game.move ({ row: 1, col: 0 })
+    # player 1 move
+    game.move ({ row: 0, col: 1 })
+    # player 2 move
+    game.move ({ row: 1, col: 1 })
+    # player 1 move, win!
+    game.move ({ row: 0, col: 2 })
+  end
+
   describe "#name" do
     it "returns the name of game" do
       expect(game.name).to eq("Tic-Tac-Toe")
@@ -45,17 +58,7 @@ describe Game, type: :model do
 
     context "when player 1 is the winner" do
       it "returns player 1" do
-        # player 1 move
-        game.move ({ row: 0, col: 0 })
-        # player 2 move
-        game.move ({ row: 1, col: 0 })
-        # player 1 move
-        game.move ({ row: 0, col: 1 })
-        # player 2 move
-        game.move ({ row: 1, col: 1 })
-        # player 1 move, win!
-        game.move ({ row: 0, col: 2 })
-
+        move_to_player_1_win
         expect(game.winning_player).to eq(game.player_1)
       end
     end
@@ -73,4 +76,19 @@ describe Game, type: :model do
     end
   end
 
+  describe "#waiting_for_turn?" do
+    it "returns true if the current player is waiting for their turn" do
+      expect(game.waiting_for_turn?(game.player_2)).to be_truthy
+    end
+
+    it "returns false if it is the current player's turn" do
+      expect(game.waiting_for_turn?(game.player_1)).to be_falsey
+    end
+
+    it "returns false if the game is over" do
+      move_to_player_1_win
+      expect(game.waiting_for_turn?(game.player_1)).to be_falsey
+      expect(game.waiting_for_turn?(game.player_2)).to be_falsey
+    end
+  end
 end
